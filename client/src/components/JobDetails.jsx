209b8e096ac1b8a9785e559f4afba850041b7841
Koushik Saha <93317898+Gituatu@users.noreply.github.com>
2024-06-22 20:23:50 +0530

    Update JobDetails.jsx

--- client/src/components/JobDetails.jsx
+++ client/src/components/JobDetails.jsx
@@ -17,7 +17,7 @@ const JobDetails = () => {
   const [job, setJob] = useState(null);
 
   useEffect(() => {
-    fetch(`https://test-bz7hejocd-gituatus-projects.vercel.app/job/${id}`)
+    fetch(`https://test-rho-five-19.vercel.app/job/${id}`)
       .then((res) => res.json())
       .then((data) => {
         setJob(data.job);
