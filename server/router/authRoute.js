275fe14a4e4589ba3e4f4ecb55210cad113d7b78
Koushik Saha <93317898+Gituatu@users.noreply.github.com>
2024-06-22 19:46:45 +0530

    Add files via upload

---
+++ server/router/authRoute.js
@@ -0,0 +1,31 @@
+import express from "express";
+import registerController from "../controllers/registerController.js";
+import loginController from "../controllers/loginController.js";
+const authRoute = express.Router();
+import jwt from "jsonwebtoken";
+
+authRoute.post("/register", registerController);
+authRoute.post("/login", loginController);
+
+authRoute.get("/logout", (req, res) => {
+  try {
+    res.clearCookie("token", {
+      domain: "http://localhost:5173",
+    });
+    res.status(200).json("Logged out successfully");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+authRoute.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, process.env.JWT_SECRET, {}, (err, info) => {
+    if (err) {
+      res.status(401).json("Not authorized");
+    }
+    res.json(info);
+  });
+});
+
+export default authRoute;
